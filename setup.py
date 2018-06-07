1e6fc16ba6583cc13633c6ebb0499edcf07e09d9
Gregory Way <gregory.way@gmail.com>
2018-06-07 09:56:17 -0700

    add setup and init files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bsub-helper',
+      version='0.1',
+      description='custom helper to submit bsub files',
+      url='http://github.com/gwaygenomics/bsub-helper',
+      author='Gregory Way',
+      author_email='gregory.way@gmail.com',
+      license='MIT',
+      packages=['bsub-helper'],
+      zip_safe=False)
